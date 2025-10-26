d2cfa796a5a4822f04db03020f7c20fbf725bd2d
spiceharu <spices5793@gmail.com>
2025-10-26 18:20:10 +0900

    Delete src/src/pages/Admin.jsx

--- src/src/pages/Admin.jsx
+++
@@ -1,8 +0,0 @@
-export default function Admin() {
-  return (
-    <div style={{ textAlign: "center", marginTop: "80px" }}>
-      <h1>管理画面</h1>
-      <p>ここで画像やテキストの変更ができます。</p>
-    </div>
-  );
-}
