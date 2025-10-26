f6dd2f7ac5c5ca64e59a0cce06b1c9a9ad84486e
spiceharu <spices5793@gmail.com>
2025-10-26 18:20:22 +0900

    Delete src/src/pages/Home.jsx

--- src/src/pages/Home.jsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ textAlign: "center", marginTop: "80px" }}>
-      <h1>Spice Nail</h1>
-      <p>ようこそ！こちらはお客様用のトップページです。</p>
-    </div>
-  );
-}
