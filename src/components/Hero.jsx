9e14a0f020c706559c20a9d9b317d7e64665040b
spiceharu <spices5793@gmail.com>
2025-10-28 08:52:32 +0900

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,13 +1,7 @@
-// src/components/Hero.jsx
 export default function Hero({ heroUrl }) {
   return (
-    <section className="hero">
-      <img
-        src={heroUrl}
-        alt="Top"
-        className="hero-img"
-        style={{ width: '100%', height: 320, objectFit: 'cover', borderRadius: 12 }}
-      />
+    <section className="hero-wrap">
+      <img src={heroUrl} alt="Top" className="hero-img" />
     </section>
   );
 }
