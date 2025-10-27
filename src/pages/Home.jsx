72ec328321cb6c9c610a0c475b77dd711d09fb43
spiceharu <spices5793@gmail.com>
2025-10-28 08:07:06 +0900

    Refactor Hero component usage in Home page
    Updated the Hero component to directly use the image URL.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,9 @@
-// src/pages/Home.jsx
 import Hero from "../components/Hero";
-import heroDesktop from "/images/hero-desktop.png"; // ← ここで画像を読み込む
 
 export default function Home() {
-  const heroUrl = heroDesktop; // 読み込んだ画像をHeroに渡す
-
   return (
-    <div style={{ textAlign: "center", marginTop: "50px" }}>
-      {/* Heroコンポーネントを呼び出し */}
-      <Hero heroUrl={heroUrl} />
-
+    <div style={{ textAlign: "center", marginTop: 24 }}>
+      <Hero heroUrl="/images/hero-desktop.png" />
       <h1>Spice Nail へようこそ！</h1>
       <p>このページが表示されていればデプロイ成功です！</p>
     </div>
