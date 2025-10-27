20c241d52437d08b16010d2e8d024153adf7a944
spiceharu <spices5793@gmail.com>
2025-10-28 08:50:43 +0900

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,19 @@
-// src/main.jsx
+import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home.jsx';
 import Admin from './pages/Admin.jsx';
 import './style.css';
 
-createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/admin" element={<Admin />} />
-    </Routes>
-  </BrowserRouter>
-);
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/admin" element={<Admin />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+createRoot(document.getElementById('root')).render(<App />);
