018ea86bdc41662f3850306e5594cb826175f86d
spiceharu <spices5793@gmail.com>
2025-10-28 18:24:50 +0900

    Create ping.js

---
+++ api/ping.js
@@ -0,0 +1,6 @@
+// /api/ping.js
+export const config = { runtime: "nodejs" };
+
+export default async function handler(req, res) {
+  return res.status(200).json({ ok: true, time: new Date().toISOString() });
+}
