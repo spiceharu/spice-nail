d2b5b51d42a80123203c9f9538bbfa3a08559453
spiceharu <spices5793@gmail.com>
2025-10-28 18:15:48 +0900

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,7 @@
 // /src/components/Hero.jsx
 export default function Hero({ heroUrl }) {
   return (
-    <section className="hero">
+    <section className="card">
       <img src={heroUrl} alt="Top" className="hero-img" />
     </section>
   );
