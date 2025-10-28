d43a2f3b885ead977bc362efe190efc508897230
spiceharu <spices5793@gmail.com>
2025-10-28 09:55:02 +0900

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,17 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home.jsx';
-import Admin from './pages/Admin.jsx';
-import './style.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import Admin from "./pages/Admin.jsx";
+import "./style.css";
 
-function App() {
-  return (
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/admin" element={<Admin />} />
       </Routes>
     </BrowserRouter>
-  );
-}
-
-createRoot(document.getElementById('root')).render(<App />);
+  </React.StrictMode>
+);
