af92f29c1c3faa32aee69fc26957af441f2972c8
spiceharu <spices5793@gmail.com>
2025-10-30 22:01:21 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,18 @@
+// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    host: true,
+    port: 5173
+  },
+  preview: {
+    host: true,
+    port: 4173
+  },
+  build: {
+    outDir: "dist"
+  }
 });
